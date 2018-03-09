d1fb3c7690480f48a3bc2d0272110f5208010de1
Paul Carpenter <paul@pcserviceselectronics.co.uk>
2018-03-09 21:58:15 +0000

    Correct Systick and Tone
    Rewrite of Systick timer event to be able to handle many timed events with call back functions. 
    Been tested using delay (ms) and 4 tones of various duration at same time.
    Tone updated to use new scheduling and more comments

--- arm/cores/Tone.cpp
+++ arm/cores/Tone.cpp
@@ -15,167 +15,142 @@
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-
+/* Tones are Systick timer event related not hardware timer related
+   So take care in selecting frequencies range of 1 - 500 Hz supported */
 
 //****************************************************************************
 // @Project Includes
 //****************************************************************************
 #include "Arduino.h"
-#include "pins_arduino.h"
 
 //****************************************************************************
 // @Macros
 //****************************************************************************
-#define FREQUENCY_TO_MILLIS(f)   (1000/(2*f)
-#define TIMER_TO_IRQ_HANDLER(t)  t==0?TIMER_0_IRQHandler:t==1?TIMER_1_IRQHandler:t==2?TIMER_2_IRQHandler:t==3?TIMER_3_IRQHandler:NULL
+#define FREQUENCY_TO_MILLIS(f)   (SYSTIMER_TICK_PERIOD_US/(2*f))
 
 //****************************************************************************
 // @Defines
 //****************************************************************************
-// Max num of timer: 8 (- 1 for delay funct.)
-#define AVAILABLE_TONE_PINS 4
 
 //****************************************************************************
 // @Global Variables
 //****************************************************************************
-static uint8_t tone_pins[AVAILABLE_TONE_PINS] = { 255, 255, 255, 255 };
-static uint32_t timer_ids[AVAILABLE_TONE_PINS] = { 0, 0, 0, 0 };
-static long timer_toggle_count[AVAILABLE_TONE_PINS] = { 0, 0, 0, 0 };
-
-//****************************************************************************
-// @Prototypes Of Local Functions
-//****************************************************************************
-void TIMER_0_IRQHandler(void);
-void TIMER_1_IRQHandler(void);
-void TIMER_2_IRQHandler(void);
-void TIMER_3_IRQHandler(void);
-
-void timer_irq_action(uint8_t);
+// Valid Arduino pin or 255 for not allocated
+volatile uint8_t tone_pins[ NUM_TONE_PINS ];
+// Toggle counts > 0 toggles to do
+// 0 is next time stop
+// < 0 is continuous running
+volatile long timer_toggle_count[ NUM_TONE_PINS ];
+// arrays initialised
+uint8_t tone_init = 0;
 
 //****************************************************************************
 // @Local Functions
 //****************************************************************************
 
-static int8_t toneBegin(uint8_t _pin)
+static int8_t toneBegin( uint8_t _pin )
 {
-    int8_t _timer = -1;
+int8_t i = -1;
 
-    // if we're already using the pin, the timer should be configured.
-    for (int i = 0; i < AVAILABLE_TONE_PINS; i++)
-    {
-        if (tone_pins[i] == _pin)
-        {
-            return i;
-        }
-    }
+// check all arrays initialised
+if( tone_init == 0 )
+  {
+  // No preset them for number of Tine pins
+  memset( (void *)tone_pins, 255, sizeof( tone_pins ) );
+  memset( (void *)timer_toggle_count, 0, sizeof( timer_toggle_count ) );
+  tone_init++;
+  }
 
-    // search for an unused timer.
-    for (int i = 0; i < AVAILABLE_TONE_PINS; i++)
-    {
-        if (tone_pins[i] == 255)
-        {
-            tone_pins[i] = _pin;
-            pinMode(_pin, OUTPUT);
-            _timer = i;
-            break;
-        }
-    }
+// Once initialised
+// if we're already using the pin, the timer should be configured.
+for( i = 0; i < NUM_TONE_PINS; i++ )
+    if( tone_pins[ i ] == _pin)
+        return i;
 
-    return _timer;
+// search for an unused timer.
+for( i = 0; i < NUM_TONE_PINS; i++ )
+   if( tone_pins[ i ] == 255 )
+     {
+     tone_pins[ i ] = _pin;
+     pinMode( _pin, OUTPUT );
+     break;
+     }
+ 
+return ( i < NUM_TONE_PINS ) ? i : -1;
 }
+
 
 // frequency (in hertz) and duration (in milliseconds).
-void tone(uint8_t _pin, unsigned int frequency, unsigned long duration)
+// Max frequency usable = 500 Hz
+// Min frequency usable = 1 Hz
+// Duration default of 0 for permanently on
+// Timer task ID is tone pin index _timer
+void tone( uint8_t _pin, unsigned int frequency, unsigned long duration )
 {
-    long toggle_count = 0;
-    int8_t _timer;
+int8_t _timer;
 
-    _timer = toneBegin(_pin);
+// Check valid frequency range first
+if( frequency > 0 && frequency <= 500 )
+  {
+  _timer = toneBegin( _pin );
 
-    if (_timer >= 0 && _timer < AVAILABLE_TONE_PINS)
+  if( _timer >= 0 && _timer < NUM_TONE_PINS )
     {
-
-        // Calculate the toggle count
-        if (duration > 0)
-        {
-            toggle_count = 2 * frequency * duration / 1000;
-        }
-        else
-        {
-            toggle_count = -1;
-        }
-
-        timer_ids[_timer] = XMC_SYSTIMER_CreateTimer((uint32_t)FREQUENCY_TO_MILLIS(frequency)), XMC_SYSTIMER_MODE_PERIODIC, (XMC_SYSTIMER_CALLBACK_t) (TIMER_TO_IRQ_HANDLER(_timer)), NULL);
-        if (timer_ids[_timer] != 0)
-        {
-            //Timer is created successfully
-            timer_toggle_count[_timer] = toggle_count;
-            XMC_SYSTIMER_StartTimer(timer_ids[_timer]);
-        }
-    }
-}
-
-void disableTimer(uint8_t _timer)
-{
-    if (_timer >= 0 && _timer < AVAILABLE_TONE_PINS)
-    {
-        XMC_SYSTIMER_DeleteTimer(timer_ids[_timer]);
-    }
-}
-
-
-void noTone(uint8_t _pin)
-{
-    int8_t _timer = -1;
-
-    for (int i = 0; i < AVAILABLE_TONE_PINS; i++)
-    {
-        if (tone_pins[i] == _pin)
-        {
-            _timer = i;
-            tone_pins[i] = 255;
-            break;
-        }
-    }
-
-    disableTimer(_timer);
-
-    digitalWrite(_pin, 0);
-}
-
-void TIMER_0_IRQHandler(void)
-{
-    timer_irq_action(0);
-}
-
-void TIMER_1_IRQHandler(void)
-{
-    timer_irq_action(1);
-}
-
-void TIMER_2_IRQHandler(void)
-{
-    timer_irq_action(2);
-}
-
-void TIMER_3_IRQHandler(void)
-{
-    timer_irq_action(3);
-}
-
-void timer_irq_action(uint8_t _timer)
-{
-    if (timer_toggle_count[_timer])
-    {
-        XMC_GPIO_ToggleOutput(mapping_port_pin[tone_pins[_timer]].port, mapping_port_pin[tone_pins[_timer]].pin);
-        timer_toggle_count[_timer]--;
-    }
+    // Calculate the toggle count
+    if( duration > 0 )
+      timer_toggle_count[ _timer ] = 2 * frequency * duration/1000;
     else
-    {
-        disableTimer(_timer);
-        timer_ids[_timer] = 0;
-        digitalWrite(tone_pins[_timer], LOW);
+      timer_toggle_count[ _timer ] = -1;        // Continuous
+    // Set int task parameters
+    setParam( _timer, _timer );
+    setInterval( _timer, (unsigned int)FREQUENCY_TO_MILLIS( frequency ) );
+    StartTask( _timer );
     }
+  }
+}
+
+
+// Stops tone at next 1ms Systick
+void noTone( uint8_t _pin )
+{
+int8_t i = -1;
+
+for( i = 0; i < NUM_TONE_PINS; i++ )
+   if( tone_pins[ i ] == _pin )
+     {
+     tone_pins[ i ] = 255;
+     break;
+     }
+
+if( i < NUM_TONE_PINS )   // Only if found set to stop at max next Systick event
+  {    
+  noInterrupts( );              // Interrupt disable guard to be safe
+  timer_toggle_count[ i ] = 0;  // Stop on next Systick
+  setInterval( i, 1 );
+  interrupts( );
+  }
+}
+
+
+/* Interrupt GPIO action
+     On normal operation to continue returns 1
+     On end of operations returns 0 to stop tasks   
+*/
+int tone_irq_action( int ID, int16_t tone )
+{
+if( timer_toggle_count[ tone ] )
+  {
+  digitalToggle( tone_pins[ tone ] );
+  if( timer_toggle_count[ tone ] > 0 )
+    timer_toggle_count[ tone ]--;
+  }
+else
+  {
+  setInterval( ID, 0 );         // no longer wanted to run
+  digitalWrite( tone_pins[ tone ], LOW );
+  return 0;
+  }
+return 1;  
 }
 
 //****************************************************************************
