db2efd4b35bf5406fa67a802de6b6163542359b1
Hollfelder Manuel (IFAG DES TCP PCB) <Manuel.Hollfelder@infineon.com>
2018-03-14 22:15:20 +0100

    Added note of changed files
    Added note of changed files to the license header:
    
    Copyright (c) 2018 Infineon Technologies AG
    This file has been modified for the XMC microcontroller series.

--- arm/variants/XMC1300/config/XMC1300_Boot_Kit/pins_arduino.h
+++ arm/variants/XMC1300/config/XMC1300_Boot_Kit/pins_arduino.h
@@ -18,6 +18,9 @@
   Public License along with this library; if not, write to the
   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
   Boston, MA  02111-1307  USA
+  
+  Copyright (c) 2018 Infineon Technologies AG
+  This file has been modified for the XMC microcontroller series.
 */
 
 #ifndef PINS_ARDUINO_H_
--- arm/variants/XMC4800/config/XMC4800_Relax_Kit/pins_arduino.h
+++ arm/variants/XMC4800/config/XMC4800_Relax_Kit/pins_arduino.h
@@ -1,24 +1,28 @@
 /*
-pins_arduino.h - Pin definition functions for Arduino
-Part of Arduino - http://www.arduino.cc/
+  pins_arduino.h - Pin definition functions for Arduino
+  Part of Arduino - http://www.arduino.cc/
 
-Copyright (c) 2007 David A. Mellis
+  Copyright (c) 2007 David A. Mellis
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
 
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
 
-You should have received a copy of the GNU Lesser General
-Public License along with this library; if not, write to the
-Free Software Foundation, Inc., 59 Temple Place, Suite 330,
-Boston, MA  02111-1307  USA
+  You should have received a copy of the GNU Lesser General
+  Public License along with this library; if not, write to the
+  Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+  Boston, MA  02111-1307  USA
+  
+  Copyright (c) 2018 Infineon Technologies AG
+  This file has been modified for the XMC microcontroller series.
 */
+
 #include <XMC4800.h>
 #ifndef PINS_ARDUINO_H_
 #define PINS_ARDUINO_H_
