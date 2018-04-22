8f6c35f54bfc9d2c8ca0840e4aecdff61af926d0
Paul Carpenter <paul@pcserviceselectronics.co.uk>
2018-04-22 21:42:44 +0100

    Improve DeviceControlXMC library and examples
    Tidy files
    Remove function freeRAM_Stack from library too much overhead
    Add constant total_heap_s
    Improve examples more actions and better comments, make DieMeasurementm  StackMeamorymeasurement and HeapMemoryMeasurement boatrd agnostic
    
    RTS/Sleep mode examples need to wait for RTC examples rewrite as too much bloats or duplicates XMCLib

--- arm/libraries/DeviceControlXMC/examples/HeapMemoryMeasurement/HeapMemoryMeasurement.ino
+++ arm/libraries/DeviceControlXMC/examples/HeapMemoryMeasurement/HeapMemoryMeasurement.ino
@@ -2,49 +2,79 @@
   Simple example to check heap memory during run time for XMC devices
   Demonstrates the ability to check on free heap memory.
   15 July 2017
+  
+  Amended April 2018 Paul Carpenter, PC Services
+  Needs first serial port to display results at 115,200 baud
+  
+  Add sign on header and total heap at compile time
+  Add details for malloc and free after new() and delete[]() of array done
+  Simplify code
+  Make board agnostic
+  
+  At best gives largest free chunk available depending on any other
+  malloc/free usage in your application.
  */
 #include <DeviceControlXMC.h>
 
-XMCClass xmc2go;
+XMCClass devCtrl;
 
 char *pointer;        // Points to allocated memory 
 size_t  free_memory;  // Used to store the value of unused memory;
 
-void setup()
-{
-  Serial.begin(9600);
 
-}
-void loop()
+// Gets amount of free heap and sends to serial port as decimal number
+void dispFree( )
 {
+  free_memory = devCtrl.freeRAM_Heap( );
+  Serial.println( free_memory );
+}
+
+
+void setup( )
+{
+  Serial.begin( 115200 );
+  delay( 100 );
+  Serial.println( "Heap free memory example  (V2.0)" );
+  Serial.print( "Total heap at Compile time\t" );
+  Serial.println( (uint32_t)( total_heap_s ) );
   // Check the total free memory
-  
-  free_memory = xmc2go.freeRAM_Heap();
-  Serial.print("Free memory before allocation is :");
-  Serial.print(free_memory);
-  delay(100);
-  Serial.println();
+  Serial.print( "Free memory before allocation:\t" );
+  dispFree( );
   
   // Allocate some memory and check the changes
-  pointer = (char*)malloc(100);
-  if(pointer != 0)  // check if we had enough space to allocate that memory
-    Serial.print("Allocated 100 bytes");
-  delay(100);
-  free_memory = xmc2go.freeRAM_Heap();
-  delay(100);
-  Serial.print("Free memory after allocation is :");
-  Serial.print(free_memory);
-  Serial.println();
+  pointer = (char*)malloc( 100 );
+  if( pointer != NULL )  // check if we had enough space to allocate that memory
+    Serial.println( "Allocated 100 bytes" );
+  else  
+    Serial.println( "Failed to allocate memory" );
+  Serial.print( "Free memory after allocation:\t" );
+  dispFree( );
 
   // Free the allocated memory and check the memory now 
-  free(pointer);
-  Serial.print("Freed 100 bytes");
-  Serial.println();  
-  free_memory = xmc2go.freeRAM_Heap();
-  delay(100);
-  Serial.print("Free memory after freeing is :");
-  Serial.print(free_memory);
+  free( pointer );
+  Serial.println( "Freed 100 bytes" );
+  Serial.print( "Free memory after freeing:\t" );
+  dispFree( );
   
-  while(1){}
+  // use new and delete on array of 100 x 32 bit ints
+  pointer = (char *)new( int[ 100 ] );
+  if( pointer != NULL )  // check if we had enough space to allocate that memory
+    Serial.println( "new() array of integers " );
+  else  
+    Serial.println( "Failed to allocate memory" );
+  Serial.print( "Free memory after new():\t" );
+  dispFree( );
+
+  // Free the allocated memory and check the memory now 
+  delete[]( pointer );
+  Serial.println( "Delete array" );
+  Serial.print( "Free memory after delete:\t" );
+  dispFree( );
+}
+
+
+void loop( )
+{
+// do absolutely nothing
 }
 
