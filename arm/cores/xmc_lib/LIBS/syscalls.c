905d135b50bc509b66c9f407748db114eed52a20
Moritz Hoechner <moritz.hoechner@infineon.com>
2018-06-15 11:13:00 +0200

    Updated syscalls.c

--- arm/cores/xmc_lib/LIBS/syscalls.c
+++ arm/cores/xmc_lib/LIBS/syscalls.c
@@ -95,7 +95,7 @@ caddr_t _sbrk(int nbytes)
 
 /* Init */
 // This forward declaration is not compatible with Arduino and commented out
-#ifdef DAVECE
+#ifdef (DAVECE || _INIT_DECLARATION_REQUIRED)
 void _init(void)
 {}
 #endif
