4a3ecfa7b0f1ee715bb04f05499822400c2599a5
Manuel Hollfelder <Manuel.Hollfelder@infineon.com>
2018-11-04 19:24:17 +0100

    Changed xmc_i2s_conf.h
    CHANGED: moved part of the definitions to the I2S.cpp/.h file
    CHANGED: settings for rewritten I2S library

--- arm/libraries/I2S/src/utility/xmc_i2s_conf.h
+++ arm/libraries/I2S/src/utility/xmc_i2s_conf.h
@@ -1,3 +1,31 @@
+/**
+ * xmc_i2s_conf.h - Auxiliary file for the I2S library for Arduino.
+ * 
+ * Have a look at the application note/datasheet for more information.
+ * 
+ * Copyright (c) 2018 Infineon Technologies AG
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,are permitted provided that the 
+ * following conditions are met:   
+ *                                                                              
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following 
+ * disclaimer.                        
+ * 
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following 
+ * disclaimer in the documentation and/or other materials provided with the distribution.                       
+ * 
+ * Neither the name of the copyright holders nor the names of its contributors may be used to endorse or promote 
+ * products derived from this software without specific prior written permission.                                           
+ *                                                                              
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE  FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR  
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+ * WHETHER IN CONTRACT, STRICT LIABILITY,OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   
+ */
+
 #ifndef XMC_I2C_CONF_H
 #define XMC_I2C_CONF_H
 
@@ -12,7 +40,7 @@ extern "C"
 
 #define BUFFER_SIZE 512
 
-// only tested with 4700
+// Only tested with XMC4700
 /*U2C0*/
 /*DX0C -> P3.7*/
 /*SCLKOUT(ALT1) -> P3.9*/
@@ -45,14 +73,11 @@ typedef struct XMC_I2S
     XMC_GPIO_CONFIG_t input_config;
     XMC_GPIO_CONFIG_t sclk_config;
     XMC_GPIO_CONFIG_t wa_config;
-    XMC_I2S_CH_CONFIG_t i2s_ch_config;
-    IRQn_Type receive_irq_num;
-    uint32_t receive_irq_service_request;
     IRQn_Type protocol_irq_num;
     uint32_t protocol_irq_service_request;
 } XMC_I2S_t;
 
-extern XMC_I2S_t i2s_config;
+extern XMC_I2S_t i2s_config; 
 
 #ifdef __cplusplus
 }
