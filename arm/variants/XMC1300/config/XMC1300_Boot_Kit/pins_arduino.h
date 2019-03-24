e4b73d3d927ffb8856620bda70a859d95487d665
Paul Carpenter <paul@pcserviceselectronics.co.uk>
2019-03-24 23:21:33 +0000

    Extend Analog inputs on Boot kits
    Add all available channels to XMC100 Boot Kit and XMC1300  Boot Kit
    
    XMC1300 now 12 inputs
    XMC1100 now 8 inputs
    
    2 of the pins on each also I2C so use with caution

--- arm/variants/XMC1300/config/XMC1300_Boot_Kit/pins_arduino.h
+++ arm/variants/XMC1300/config/XMC1300_Boot_Kit/pins_arduino.h
@@ -40,7 +40,8 @@
 /* On board LED is ON when digital output is 0, LOW, False, OFF */
 #define  XMC_LED_ON         0
 
-#define NUM_ANALOG_INPUTS   6
+/* Modified to allow all possible analogue input pins */
+#define NUM_ANALOG_INPUTS   12
 #define NUM_PWM             4
 #define NUM_LEDS            6
 #define NUM_INTERRUPT       2
@@ -78,6 +79,12 @@
 #define A3   3
 #define A4   4
 #define A5   5
+#define A6   6
+#define A7   7
+#define A8   8
+#define A9   9
+#define A10  10
+#define A11  11
 
 #define PIN_SPI_SS_2  23
 
@@ -89,7 +96,7 @@
 #define AUX_4       29  // AUX
 #define AUX_5       30  // AUX
 
-#define LED_BUILTIN 24 	// Standard Arduino LED pin 13
+#define LED_BUILTIN 24 	// Standard Arduino LED
 #define LED1        24  // Extended LEDs P0.0
 #define LED2        25  // Extended LEDs P0.1
 #define LED3        29	// Extended LEDs P0.6
@@ -117,68 +124,75 @@ const uint8_t mapping_pin_PWM8[][ 2 ] = {
 
 const XMC_PORT_PIN_t mapping_port_pin[] =
 {
-	/* 0  */ 	{XMC_GPIO_PORT2 ,4},  // A0 / ADC Input 					P2.4 (INPUT ONLY)
-	/* 1  */ 	{XMC_GPIO_PORT2 ,5},  // A1 / ADC Input 					P2.5 (INPUT ONLY)
-	/* 2  */ 	{XMC_GPIO_PORT2 ,6},  // A2 / ADC Input						P2.6 (INPUT ONLY)
-	/* 3  */ 	{XMC_GPIO_PORT2 ,7},  // A3 / ADC Input						P2.7 (INPUT ONLY)
-	/* 4  */ 	{XMC_GPIO_PORT2 ,8},  // A4 / ADC Input						P2.8 (INPUT ONLY)
-	/* 5  */ 	{XMC_GPIO_PORT2 ,9},  // A5 / ADC Input						P2.9 (INPUT ONLY)
-	/* 6  */ 	{XMC_GPIO_PORT2 ,10}, // GPIO								P2.10
-	/* 7  */ 	{XMC_GPIO_PORT2 ,11}, // GPIO								P2.11
-	/* 8  */ 	{XMC_GPIO_PORT2 ,2},  // GPIO								P2.2 (INPUT ONLY)
-	/* 9  */ 	{XMC_GPIO_PORT2 ,3},  // GPIO								P2.3 (INPUT ONLY)
-	/* 10  */ 	{XMC_GPIO_PORT2 ,0},  // I2C Clock SCL						P2.0
-	/* 11  */ 	{XMC_GPIO_PORT2 ,1},  // I2C Data / Address SDA				P2.1
-	/* 12  */ 	{XMC_GPIO_PORT0 ,14}, // GPIO								P0.14
-	/* 13  */ 	{XMC_GPIO_PORT0 ,15}, // GPIO								P0.15
-	/* 14  */ 	{XMC_GPIO_PORT0 ,12}, // External interrupt	0				P0.12
-	/* 15  */ 	{XMC_GPIO_PORT0 ,13}, // External interrupt	1				P0.13
-	/* 16  */ 	{XMC_GPIO_PORT0 ,10}, // GPIO								P0.10
-	/* 17  */ 	{XMC_GPIO_PORT0 ,11}, // GPIO								P0.11
-	/* 18  */ 	{XMC_GPIO_PORT1 ,5},  // GPIO								P1.5
-	/* 19  */ 	{XMC_GPIO_PORT1 ,4},  // GPIO								P1.4
-	/* 20  */ 	{XMC_GPIO_PORT1 ,3},  // TX 	    			  			P1.3
-	/* 21  */ 	{XMC_GPIO_PORT1, 2},  // RX     	 						P1.2
-	/* 22  */ 	{XMC_GPIO_PORT1 ,1},  // SPI-MOSI							P1.1
-	/* 23  */ 	{XMC_GPIO_PORT1 ,0},  // SPI-MIS0							P1.0
-	/* 24  */ 	{XMC_GPIO_PORT0 ,0},  // LED output	LED1	(BUILTIN)		P0.0
-	/* 25  */ 	{XMC_GPIO_PORT0 ,1},  // LED output	LED2					P0.1
-	/* 26  */ 	{XMC_GPIO_PORT0 ,2},  // PWM (PWM4 slice 1)					P0.2
-	/* 27  */ 	{XMC_GPIO_PORT0 ,8},  // LED output		LED5				P0.8
-	/* 28  */ 	{XMC_GPIO_PORT0 ,9},  // LED output		LED6				P0.9
-	/* 29  */ 	{XMC_GPIO_PORT0 ,6},  // SPI-SS / LED output LED3			P0.6
-	/* 30  */ 	{XMC_GPIO_PORT0 ,7},  // SPI-SCK / LED output LED4			P0.7
-	/* 31  */ 	{XMC_GPIO_PORT0 ,4},  // PWM (PWM4 slice 0)					P0.4
-	/* 32  */ 	{XMC_GPIO_PORT0 ,5},  // PWM (PWM8 slice 0)					P0.5
-	/* 33  */ 	{XMC_GPIO_PORT0 ,3}   // PWM (PWM8 slice 1)				    P0.3
+	/* 0  */ 	{ XMC_GPIO_PORT2, 4 },  // A0 / ADC Input 					P2.4    (INPUT ONLY)
+	/* 1  */ 	{ XMC_GPIO_PORT2, 5 },  // A1 / ADC Input 					P2.5    (INPUT ONLY)
+	/* 2  */ 	{ XMC_GPIO_PORT2, 6 },  // A2 / ADC Input					P2.6    (INPUT ONLY)
+	/* 3  */ 	{ XMC_GPIO_PORT2, 7 },  // A3 / ADC Input					P2.7    (INPUT ONLY)
+	/* 4  */ 	{ XMC_GPIO_PORT2, 8 },  // A4 / ADC Input					P2.8    (INPUT ONLY)
+	/* 5  */ 	{ XMC_GPIO_PORT2, 9 },  // A5 / ADC Input					P2.9    (INPUT ONLY)
+	/* 6  */ 	{ XMC_GPIO_PORT2, 10 }, // GPIO / A6 ADC Input				P2.10
+	/* 7  */ 	{ XMC_GPIO_PORT2, 11 }, // GPIO / A7 ADC Input				P2.11
+	/* 8  */ 	{ XMC_GPIO_PORT2, 2 },  // GPIO / A9 ADC Input				P2.2    (INPUT ONLY)
+	/* 9  */ 	{ XMC_GPIO_PORT2, 3 },  // GPIO / A8 ADC Input				P2.3    (INPUT ONLY)
+	/* 10  */ 	{ XMC_GPIO_PORT2, 0 },  // I2C Clock SCL / A11 ADC Input	P2.0
+	/* 11  */ 	{ XMC_GPIO_PORT2, 1 },  // I2C Data  SDA / A10 ADC Input    P2.1
+	/* 12  */ 	{ XMC_GPIO_PORT0, 14 }, // GPIO								P0.14
+	/* 13  */ 	{ XMC_GPIO_PORT0, 15 }, // GPIO								P0.15
+	/* 14  */ 	{ XMC_GPIO_PORT0, 12 }, // External interrupt	0			P0.12
+	/* 15  */ 	{ XMC_GPIO_PORT0, 13 }, // External interrupt	1			P0.13
+	/* 16  */ 	{ XMC_GPIO_PORT0, 10 }, // GPIO								P0.10
+	/* 17  */ 	{ XMC_GPIO_PORT0, 11 }, // GPIO								P0.11
+	/* 18  */ 	{ XMC_GPIO_PORT1, 5 },  // GPIO								P1.5
+	/* 19  */ 	{ XMC_GPIO_PORT1, 4 },  // GPIO								P1.4
+	/* 20  */ 	{ XMC_GPIO_PORT1, 3 },  // TX 	    			  			P1.3
+	/* 21  */ 	{ XMC_GPIO_PORT1, 2 },  // RX     	 						P1.2
+	/* 22  */ 	{ XMC_GPIO_PORT1, 1 },  // SPI-MOSI							P1.1
+	/* 23  */ 	{ XMC_GPIO_PORT1, 0 },  // SPI-MIS0							P1.0
+	/* 24  */ 	{ XMC_GPIO_PORT0, 0 },  // LED output	(BUILTIN)   LED1	P0.0
+	/* 25  */ 	{ XMC_GPIO_PORT0, 1 },  // LED output	            LED2	P0.1
+	/* 26  */ 	{ XMC_GPIO_PORT0, 2 },  // PWM (PWM4 slice 1)				P0.2
+	/* 27  */ 	{ XMC_GPIO_PORT0, 8 },  // LED output	    	    LED5	P0.8
+	/* 28  */ 	{ XMC_GPIO_PORT0, 9 },  // LED output		        LED6	P0.9
+	/* 29  */ 	{ XMC_GPIO_PORT0, 6 },  // SPI-SS / LED output      LED3   	P0.6
+	/* 30  */ 	{ XMC_GPIO_PORT0, 7 },  // SPI-SCK / LED output     LED4	P0.7
+	/* 31  */ 	{ XMC_GPIO_PORT0, 4 },  // PWM (PWM4 slice 0)				P0.4
+	/* 32  */ 	{ XMC_GPIO_PORT0, 5 },  // PWM (PWM8 slice 0)				P0.5
+	/* 33  */ 	{ XMC_GPIO_PORT0, 3 }   // PWM (PWM8 slice 1)			    P0.3
 };
 
 const XMC_PIN_INTERRUPT_t mapping_interrupt[] =
 {
-    /* 0  */    {CCU40, CCU40_CC40, 0, 0, CCU40_IN0_P0_12},
-    /* 1  */    {CCU40, CCU40_CC40, 0, 1, CCU40_IN0_U0C0_DX2INS}
+    /* 0  */    { CCU40, CCU40_CC40, 0, 0, CCU40_IN0_P0_12 },
+    /* 1  */    { CCU40, CCU40_CC40, 0, 1, CCU40_IN0_U0C0_DX2INS }
 };
 
 XMC_PWM4_t mapping_pwm4[] =
 {
-    {CCU40, CCU40_CC41, 1, mapping_port_pin[31], P0_4_AF_CCU40_OUT1, XMC_CCU4_SLICE_PRESCALER_64, PWM4_TIMER_PERIOD, DISABLED}, // PWM disabled  31  P0.4
-    {CCU40, CCU40_CC42, 2, mapping_port_pin[26], P0_2_AF_CCU40_OUT2, XMC_CCU4_SLICE_PRESCALER_64, PWM4_TIMER_PERIOD, DISABLED}  // PWM disabled  26  P0.2
+    { CCU40, CCU40_CC41, 1, mapping_port_pin[ 31 ], P0_4_AF_CCU40_OUT1, XMC_CCU4_SLICE_PRESCALER_64, PWM4_TIMER_PERIOD, DISABLED }, // PWM disabled  31  P0.4
+    { CCU40, CCU40_CC42, 2, mapping_port_pin[ 26 ], P0_2_AF_CCU40_OUT2, XMC_CCU4_SLICE_PRESCALER_64, PWM4_TIMER_PERIOD, DISABLED }  // PWM disabled  26  P0.2
 };
 
 XMC_PWM8_t mapping_pwm8[] =
 {
-    {CCU80, CCU80_CC81, 1, XMC_CCU8_SLICE_COMPARE_CHANNEL_2, mapping_port_pin[32], P0_5_AF_CCU80_OUT12, XMC_CCU8_SLICE_PRESCALER_64, PWM8_TIMER_PERIOD , DISABLED},  // PWM disabled  32   P0.5
-    {CCU80, CCU80_CC80, 0, XMC_CCU8_SLICE_COMPARE_CHANNEL_2, mapping_port_pin[33], P0_3_AF_CCU80_OUT03, XMC_CCU8_SLICE_PRESCALER_64, PWM8_TIMER_PERIOD , DISABLED}   // PWM disabled  33   P0.3
+    { CCU80, CCU80_CC81, 1, XMC_CCU8_SLICE_COMPARE_CHANNEL_2, mapping_port_pin[ 32 ], P0_5_AF_CCU80_OUT12, XMC_CCU8_SLICE_PRESCALER_64, PWM8_TIMER_PERIOD, DISABLED },  // PWM disabled  32   P0.5
+    { CCU80, CCU80_CC80, 0, XMC_CCU8_SLICE_COMPARE_CHANNEL_2, mapping_port_pin[ 33 ], P0_3_AF_CCU80_OUT03, XMC_CCU8_SLICE_PRESCALER_64, PWM8_TIMER_PERIOD, DISABLED }   // PWM disabled  33   P0.3
 };
 
 XMC_ADC_t mapping_adc[] =
 {
-    {VADC, 6, VADC_G1, 1, 4 , DISABLED},
-    {VADC, 7, VADC_G1, 1, 11, DISABLED},
-    {VADC, 0, VADC_G0, 0, 9	, DISABLED},
-    {VADC, 1, VADC_G1, 1, 12, DISABLED},
-    {VADC, 1, VADC_G0, 0, 10, DISABLED},
-    {VADC, 2, VADC_G0, 0, 7 , DISABLED}
+    { VADC, 6, VADC_G1, 1, 4, DISABLED },
+    { VADC, 7, VADC_G1, 1, 11, DISABLED },
+    { VADC, 0, VADC_G0, 0, 9, DISABLED },
+    { VADC, 1, VADC_G1, 1, 12, DISABLED },
+    { VADC, 1, VADC_G0, 0, 10, DISABLED },
+    { VADC, 2, VADC_G0, 0, 7, DISABLED },
+    // Additional channels added here
+    { VADC, 3, VADC_G0, 0, 5, DISABLED },
+    { VADC, 4, VADC_G0, 0, 1, DISABLED },
+    { VADC, 5, VADC_G1, 1, 2, DISABLED },
+    { VADC, 7, VADC_G0, 0, 3, DISABLED },
+    { VADC, 6, VADC_G0, 0, 6, DISABLED },
+    { VADC, 5, VADC_G0, 0, 8, DISABLED }
 };
 
 /*
