00747b9f3fea2bb8fb5d3971069d856087d625a0
Daniel Stuart <daniel.stuart@pucpr.edu.br>
2020-07-07 21:00:32 -0300

    Servo: Change delay to 20ms

--- libraries/Servo/examples/Knob/Knob.ino
+++ libraries/Servo/examples/Knob/Knob.ino
@@ -27,5 +27,5 @@ void loop() {
     val = analogRead(potpin); // reads the value of the potentiometer
     val = map(val, 0, getAnalogReadMaximum(), 0, 180); // scale it to use it with the servo (value between 0 and 180)
     myservo.write(val); // sets the servo position according to the scaled value
-    delay(15); // waits for the servo to get there
+    delay(20); // waits for the servo to get there
 }
--- libraries/Servo/examples/Sweep/Sweep.ino
+++ libraries/Servo/examples/Sweep/Sweep.ino
@@ -26,11 +26,11 @@ void loop() {
     for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
         // in steps of 1 degree
         myservo.write(pos); // tell servo to go to position in variable 'pos'
-        delay(15); // waits 15ms for the servo to reach the position
+        delay(20); // waits 20ms for the servo to reach the position
     }
     
     for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
         myservo.write(pos); // tell servo to go to position in variable 'pos'
-        delay(15); // waits 15ms for the servo to reach the position
+        delay(20); // waits 20ms for the servo to reach the position
     }
 }
