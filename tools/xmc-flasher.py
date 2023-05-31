fb3248f5da7766e190c8711ac739b3f3fbb93c37
Bora Monideep (IFAG DES DOS SW ESW EPE) <Monideep.Bora@infineon.com>
2023-05-31 22:48:37 +0200

    added changes for device and mem check

--- tools/xmc-flasher.py
+++ tools/xmc-flasher.py
@@ -40,6 +40,15 @@ def create_jlink_loadbin_command_file(binfile):
 
     return cmd_jlink
 
+def create_jlink_mem_read_command_file(addr, bytes):
+    cmd_jlink = 'cmd.jlink'
+    #cmd_log_enable
+    cmd_mem_read = 'mem ' + addr +', '+ bytes  + '\n' # todo: store register maps, bytes in file
+    with open(cmd_jlink,'w') as f:
+        f.writelines([cmd_mem_read, 'r\n', 'g\n', 'exit\n'])
+
+    return cmd_jlink
+
 def create_jlink_erase_command_file():
     cmd_jlink = 'cmd.jlink'
     with open(cmd_jlink,'w') as f:
@@ -51,14 +60,50 @@ def remove_jlink_command_file(cmd_file):
     if os.path.exists(cmd_file):
         os.remove(cmd_file)
 
-def jlink_commander(device, serial_num, cmd_file):
+def jlink_commander(device, serial_num, cmd_file, console_output=False):
     jlink_cmd = [jlinkexe, '-autoconnect', '1','-exitonerror', '1', '-nogui', '1', '-device', device, '-selectemubysn', serial_num, '-if', 'swd', '-speed', '4000', '-commandfile', cmd_file]
 
+    #if console_output is True:
+        #jlink_cmd.append('-log ') #todo: pipe this in a better way
+
     try:
-        jlink_proc = subprocess.Popen(jlink_cmd)
+        jlink_proc = subprocess.Popen(jlink_cmd, stdout=subprocess.PIPE, universal_newlines=True)
+        if console_output is True:
+            out, err = jlink_proc.communicate()
+            with open('console.output', 'w') as f:
+                f.write(out)
         jlink_proc.wait()
     except:
         raise Exception("jlink error")
+    
+def process_console_output(addr):
+    with open('console.output','r') as f:
+        lines = f.readlines()
+        lines[0].split('\n')
+       
+    for line in lines :
+        if addr in line and '=' in line:
+            print(line)
+
+    
+def check_device(device, port):
+
+    device_reg_addr = '40010004'
+    device_reg_bytes = '4'
+
+    serial_num = get_device_serial_number(port)
+    jlink_cmd_file = create_jlink_mem_read_command_file(device_reg_addr, device_reg_bytes) # todo: comes from proper metafile
+    jlink_commander(device, serial_num, jlink_cmd_file, True)
+    remove_jlink_command_file(jlink_cmd_file)
+
+    process_console_output(device_reg_addr)
+
+
+
+def check_mem(device, port):
+    jlink_cmd_file = create_jlink_loadbin_command_file(binfile)
+    jlink_commander(device, serial_num, jlink_cmd_file)
+    remove_jlink_command_file(jlink_cmd_file)        
 
 def upload(device, port, binfile):
     serial_num = get_device_serial_number(port)
@@ -81,7 +126,9 @@ def parser():
         parser.print_help()
 
     def parser_upload_func(args):
-        upload(args.device, args.port, args.binfile)
+        check_device(args.device, args.port)
+        #check_mem(args.device, args.port)
+        #upload(args.device, args.port, args.binfile)
 
     def parser_erase_func(args):
         erase(args.device, args.port)
