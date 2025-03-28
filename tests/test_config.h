c7bf27b6fd55b5fca7ff3dce564fdbfeb75bf21e
Frederikwag <frederik.wagner@infineon.com>
2025-03-28 16:24:17 +0100

    Working Test for Arudino

---
+++ tests/test_config.h
@@ -0,0 +1,19 @@
+/**
+ * @file test_config.h
+ * @brief Configuration file for board-specific test pin definitions.
+ *
+ * This header file contains the definitions of the pins used for testing
+ * purposes on the specific board. These pins are configured as output and 
+ * input pins for various test scenarios.
+ *
+ */
+ #ifndef TEST_CONFIG_H
+ #define TEST_CONFIG_H
+ 
+ #include <stdint.h>
+ 
+ // test defines
+ const uint8_t TEST_DIGITALIO_OUTPUT = 4;  // IO0
+ const uint8_t TEST_DIGITALIO_INPUT = 2;   // IO1
+ 
+ #endif // TEST_CONFIG_H
