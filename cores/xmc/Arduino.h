455175e545de6fafd0eb93cc3d51e3f4628d4805
MDin <Dinesh.M-EE@infineon.com>
2025-05-12 10:18:22 +0530

    cores/xmc/: Comment serial functions.
    Signed-off-by: MDin <Dinesh.M-EE@infineon.com>

--- cores/xmc/Arduino.h
+++ cores/xmc/Arduino.h
@@ -341,9 +341,9 @@ extern void loop(void);
 
 #ifdef __cplusplus
 } // extern "C"
-    // #include "Tone.h"
-    // #include "WMath.h"
-    #include "Uart.h"
+  // #include "Tone.h"
+  // #include "WMath.h"
+  //  #include "Uart.h"
 #endif // __cplusplus
 
 // #include "Print.h"
