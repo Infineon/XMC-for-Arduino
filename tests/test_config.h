221f79d0f2ba05300f8c15f825202d222ea96b0b
zhanglinjing <Linjing.Zhang@infineon.com>
2025-06-10 17:16:38 +0200

    tests/test_config.h: Update for analogio tests.
    Signed-off-by: zhanglinjing <Linjing.Zhang@infineon.com>

--- tests/test_config.h
+++ tests/test_config.h
@@ -13,7 +13,17 @@
 #include <stdint.h>
 
 // Test Pin Definitions
-#define TEST_PIN_DIGITAL_IO_OUTPUT 2 // IO_4
-#define TEST_PIN_DIGITAL_IO_INPUT 3  // IO_3
+#define TEST_PIN_DIGITAL_IO_OUTPUT 3 // IO_4
+#define TEST_PIN_DIGITAL_IO_INPUT 2  // IO_3
 #define TEST_PIN_SYNC_IO 4           // IO_1
-#endif                               // TEST_CONFIG_H
+
+#define PWM_FREQUENCY_HZ 490          // PWM Frequency in Hz
+#define TEST_PIN_ANALOG_IO_VREF A2    // Pin connected to Vdd
+#define TEST_PIN_ANALOG_IO_DIVIDER A1 // Pin connected to voltage divider
+#define TEST_PIN_ANALOG_IO_GND A0     // Pin connected to Ground
+#define TEST_ADC_RESOLUTION 1023      // ADC resolution
+
+#define TEST_PIN_ANALOG_IO_DAC 53
+#define TEST_PIN_ANALOG_IO_DAC_INPUT A3
+
+#endif // TEST_CONFIG_H
