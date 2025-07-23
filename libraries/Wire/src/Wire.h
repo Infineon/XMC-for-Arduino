44f98634c79c43218093c5a9b5cfeb85313b1e55
MDin <Dinesh.M-EE@infineon.com>
2025-07-23 15:21:19 +0530

    libraries/Wire: Removed comments.
    Signed-off-by: MDin <Dinesh.M-EE@infineon.com>

--- libraries/Wire/src/Wire.h
+++ libraries/Wire/src/Wire.h
@@ -21,7 +21,6 @@ public:
     uint8_t endTransmission(bool);
     size_t  requestFrom(uint8_t, size_t);
     size_t  requestFrom(uint8_t, size_t, bool);
-    // size_t  requestFrom(uint8_t, size_t, uint32_t, uint8_t, bool);
     virtual size_t write(uint8_t);
     virtual size_t write(const uint8_t *, size_t);
      int available(void);
