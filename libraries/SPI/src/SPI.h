24cda12a00fd3e08edad2d5ad4c785ee6aef0fdc
MDin <Dinesh.M-EE@infineon.com>
2025-08-07 13:52:00 +0530

    libraries/SPI: Refactored SPI.
    Signed-off-by: MDin <Dinesh.M-EE@infineon.com>

--- libraries/SPI/src/SPI.h
+++ libraries/SPI/src/SPI.h
@@ -1,57 +1,26 @@
 /*
- * SPI Master library.
- * Copyright (c) 2015 Arduino LLC
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
- * Copyright (c) 2018 Infineon Technologies AG
- * This library has been modified for the XMC microcontroller series.
+ * SPI Master library for XMC microcontrollers.
+ * Inherits from Arduino's HardwareSPI.
  */
 
 #ifndef _SPI_H_INCLUDED
 #define _SPI_H_INCLUDED
 
-//****************************************************************************
-// @Project Includes
-//****************************************************************************
 #include <Arduino.h>
+#include <api/HardwareSPI.h> // Base HardwareSPI class provided by Arduino core for XMC
 
-//****************************************************************************
-// @Defines
-//****************************************************************************
-// Define USE_SW_SPI to use the software SPI(not verified)
-// #define USE_SW_SPI
-
-// Define USE_XMC_RELAX_KIT_SD allows to use the SD Lib to communicate with a SD Card over
-// the on-board SD Card slot. This feature is only available on KIT_XMC47_RELAX kits.
-#if defined(KIT_XMC47_RELAX)
-    #define USE_XMC_RELAX_KIT_SD
-#endif
-
+// SPI modes
 #define SPI_MODE0 0x00
 #define SPI_MODE1 0x01
 #define SPI_MODE2 0x02
 #define SPI_MODE3 0x03
 
-#define SPI_IMODE_NONE 0x00
-
+// Bit order definitions
 #define MSBFIRST 0x00
 #define LSBFIRST 0x01
 
+// SPI Clock Dividers
 #define ARDUINO_SPI_CLOCK 16000000U
-
 #define SPI_CLOCK_DIV2 2
 #define SPI_CLOCK_DIV4 4
 #define SPI_CLOCK_DIV8 8
@@ -60,157 +29,40 @@
 #define SPI_CLOCK_DIV64 64
 #define SPI_CLOCK_DIV128 128
 
-#if defined(USE_SW_SPI)
-    #include "SW_SPI.h"
-#endif
-
-// #define pin_cs 	10
-//
-// #if defined(KIT_XMC_2GO_XMC1100_V1) || defined(XMC1100_H_BRIDGE2GO)
-// #define pin_cs  3
-
-// #elif defined(KIT_XMC_PLT2GO_XMC4200)
-/// #define pin_cs_slot_1  95
-// #define pin_cs_slot_2  96
-
-// #endif
-
-//****************************************************************************
-// @Class Definitions
-//****************************************************************************
-class SPISettings {
+// Custom SPI implementation class
+class XMC_SPIClass : public arduino::HardwareSPI {
 public:
-    SPISettings(uint32_t cF, uint8_t bO, uint8_t dM) {
-        clockFreq = cF;
-        bitOrder = bO;
-        dataMode = dM;
-    }
+    // Constructor
+    XMC_SPIClass(XMC_SPI_t *spiConfig);
 
-    SPISettings() {
-        clockFreq = 4000000;
-        bitOrder = MSBFIRST;
-        dataMode = SPI_MODE0;
-    }
+    // Explicit empty destructor
+    ~XMC_SPIClass();
 
-    uint32_t clockFreq;
-    uint8_t bitOrder;
-    uint8_t dataMode;
-};
+    // Implement required methods from HardwareSPI
+    void begin() override;
+    void end() override;
+    uint8_t transfer(uint8_t data) override;
+    uint16_t transfer16(uint16_t data) override;
+    void transfer(void *buf, size_t count) override;
 
-class SPIClass {
-public:
-// Constructors for HW SPI
-#if !defined(USE_SW_SPI)
-    SPIClass() {
-        // default SPI
-        XMC_SPI_config = &XMC_SPI_default;
-    }
+    // Interrupt-related methods
+    void attachInterrupt() override;
+    void detachInterrupt() override;
 
-    SPIClass(XMC_SPI_t *conf) { XMC_SPI_config = conf; }
-
-// Constructors for SW SPI
-#else
-    SPIClass() {
-        // default SPI
-        spi_settings = SPISettings();
-
-        mosi_pin = MOSI;
-        miso_pin = MISO;
-        clk_pin = SCK;
-    }
-
-    SPIClass(SPISettings settings) {
-        spi_settings = settings;
-
-        mosi_pin = MOSI;
-        miso_pin = MISO;
-        clk_pin = SCK;
-    }
-
-    SPIClass(SPISettings settings, uint8_t mosi, uint8_t miso, uint8_t sck) {
-        spi_settings = settings;
-
-        mosi_pin = mosi;
-        miso_pin = miso;
-        clk_pin = sck;
-    }
-#endif
-
-    uint8_t transfer(uint8_t data);
-    inline uint16_t transfer16(uint16_t data);
-    inline void transfer(void *buf, size_t count);
-
-    // Transaction Functions
-    // Function not used here
-    // void usingInterrupt(int interruptNumber);
-    void beginTransaction(SPISettings settings);
-    void endTransaction(void);
-
-    // SPI Configuration methods
-    void attachInterrupt();
-    void detachInterrupt();
-
-    static void usingInterrupt(uint8_t interruptNumber) {};
-    static void notUsingInterrupt(uint8_t interruptNumber) {};
-
-    void begin();
-    void end();
-
-    void setBitOrder(uint8_t order);
-    void setDataMode(uint8_t mode);
-    void setClockDivider(uint8_t div);
+    // Transaction-related methods
+    void usingInterrupt(int interruptNumber) override;
+    void notUsingInterrupt(int interruptNumber) override;
+    void beginTransaction(arduino::SPISettings settings) override;
+    void endTransaction() override;
 
 private:
-    void init();
-// Config for HW SPI
-#if !defined(USE_SW_SPI)
+    void init(); // Internal initialization logic
 
-    XMC_SPI_t *XMC_SPI_config;
-
-// Config for SW SPI
-#else
-    uint8_t mosi_pin;
-    uint8_t miso_pin;
-    uint8_t clk_pin;
-
-    SPISettings spi_settings;
-#endif
-
+    XMC_SPI_t *XMC_SPI_Config; // XMC-specific SPI configuration
     bool initialized;
-    uint8_t interruptMode;
-    char interruptSave;
-    uint32_t interruptMask;
 };
 
-extern SPIClass SPI;
-#if (NUM_SPI > 1)
-extern SPIClass SPI1;
-    #if (NUM_SPI > 2)
-extern SPIClass SPI2;
-        #if (NUM_SPI > 3)
-extern SPIClass SPI3;
-            #if (NUM_SPI > 4)
-extern SPIClass SPI4;
-            #endif
-        #endif
-    #endif
-#endif
-
-void SPIClass::transfer(void *buf, size_t count) {
-    uint8_t *buffer = (uint8_t *)buf;
-    for (size_t index = 0; index < count; index++) {
-        buffer[index] = transfer(buffer[index]);
-    }
-}
-
-uint16_t SPIClass::transfer16(uint16_t data) {
-    uint8_t data_out_msb = (uint8_t)((data >> 8) & 0xFF);
-    uint8_t data_out_lsb = (uint8_t)(data & 0xFF);
-
-    uint8_t data_in_msb = transfer(data_out_msb);
-    uint8_t data_in_lsb = transfer(data_out_lsb);
-
-    return (uint16_t)(((uint16_t)data_in_msb << 8) | (data_in_lsb));
-}
+// Declare the global SPI object
+extern XMC_SPIClass SPI;
 
 #endif /* _SPI_H_INCLUDED */
