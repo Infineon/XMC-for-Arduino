e114970a55fe3b056b4b749911d85a66607a3e9f
MDin <Dinesh.M-EE@infineon.com>
2025-08-28 16:26:40 +0530

    variants: Review comments work.
    Signed-off-by: MDin <Dinesh.M-EE@infineon.com>

--- variants/XMC4700/config/KIT_XMC47_RELAX/pins_arduino.h
+++ variants/XMC4700/config/KIT_XMC47_RELAX/pins_arduino.h
@@ -40,9 +40,7 @@
 
 // Define USE_XMC_RELAX_KIT_SD allows to use the SD Lib to communicate with a SD Card over
 // the on-board SD Card Slot. This feature is only available on XMC4700 RelaxKits.
-#if defined(XMC4700_Relax_Kit)
-    #define USE_XMC_RELAX_KIT_SD
-#endif
+#define USE_XMC_RELAX_KIT_SD
 /* On board LED is ON when digital output is 1, HIGH, TRUE, ON */
 #define XMC_LED_ON 1
 
