3b0438e21803b7576cdcfff4b4706129f863ea23
MDin <Dinesh.M-EE@infineon.com>
2025-09-02 10:44:27 +0530

    cores/xmc: Revert back to older implementation.
    Signed-off-by: MDin <Dinesh.M-EE@infineon.com>

--- cores/xmc/Tone.h
+++ cores/xmc/Tone.h
@@ -1,36 +1,18 @@
-#ifndef TONE_H
-#define TONE_H
-
+#ifndef _WIRING_TONE_
+#define _WIRING_TONE_
 #include "Arduino.h"
-// Expose C-compatible declarations for wiring_time.c
+
+//****************************************************************************
+// @External Prototypes
+//****************************************************************************
+// extern void noTone(uint8_t);
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-// C-compatible function declaration for the callback
-int tone_irq_action(int taskId, int16_t param);
-
+extern int tone_irq_action(int, int16_t);
 #ifdef __cplusplus
-} // End of extern "C"
-
-class Tone {
-public:
-    Tone();
-    ~Tone();
-
-    void play(pin_size_t pin, unsigned int frequency);
-    void play(pin_size_t pin, unsigned int frequency, unsigned long duration);
-    void stop(pin_size_t pin);
-
-    static int tone_irq_action(int taskId, int16_t param);
-
-private:
-    void configureTone(pin_size_t pin, unsigned int frequency);
-    void scheduleToneTask(pin_size_t pin, unsigned long duration);
-    int scanMapTable(const uint8_t table[][2], pin_size_t pin);
-};
-
-extern Tone inst_Tone;
-
+}
 #endif
-#endif
+
+#endif /* _WIRING_TONE_ */
