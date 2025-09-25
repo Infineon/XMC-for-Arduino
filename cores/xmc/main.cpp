9c03e87578eeb6e4b26a8cf9fd63c2b07ded66a2
MDin <Dinesh.M-EE@infineon.com>
2025-09-25 14:36:48 +0530

    cores/xmc: Declared sbrk function.
    Signed-off-by: MDin <Dinesh.M-EE@infineon.com>

--- cores/xmc/main.cpp
+++ cores/xmc/main.cpp
@@ -26,6 +26,12 @@
 //****************************************************************************
 #include "Arduino.h"
 
+// work around to use new operator
+extern "C" void *_sbrk(int incr);
+void dummy_sbrk_caller() __attribute__((__used__));
+
+void dummy_sbrk_caller() { _sbrk(0); }
+
 // Weak empty variant initialization function.
 // May be redefined by variant files.
 void initVariant() __attribute__((weak));
