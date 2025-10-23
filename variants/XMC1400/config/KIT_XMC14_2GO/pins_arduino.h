e220118a151d4e1523bdce755e2998fb0d0919f3
MDin <Dinesh.M-EE@infineon.com>
2025-10-23 11:02:56 +0530

    variants: Added macro for reset.
    Signed-off-by: MDin <Dinesh.M-EE@infineon.com>

--- variants/XMC1400/config/KIT_XMC14_2GO/pins_arduino.h
+++ variants/XMC1400/config/KIT_XMC14_2GO/pins_arduino.h
@@ -58,6 +58,8 @@ extern const uint8_t NUM_ANALOG_INPUTS;
 
 // Indicate unit has RTC/Alarm
 #define HAS_RTC 1
+// Indicate variant has a GPIO pin used for Reset pin
+#define HAS_GPIO_RESET 1
 
 // Defines will be either set by ArduinoIDE in the menu or manually
 #ifdef SERIAL_HOSTPC
