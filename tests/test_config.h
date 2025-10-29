ef2b68a06ca44e0a8de310afe65f629b1f7e32bd
MDin <Dinesh.M-EE@infineon.com>
2025-10-29 17:05:20 +0530

    tests: Modified 1100 pins.
    Signed-off-by: MDin <Dinesh.M-EE@infineon.com>

--- tests/test_config.h
+++ tests/test_config.h
@@ -48,6 +48,20 @@ static const float test_pwm_frequencies[] = {1, 50, 5000, 50000};
         #define TEST_ADC_MAX_VALUE 1023
         #define TEST_ADC_RESOLUTION 10 // ADC resolution
 static const float test_pwm_frequencies[] = {1, 50, 5000, 20000};
+    #elif defined(KIT_XMC11_BOOT_001)
+        #define TEST_PIN_DIGITAL_IO_OUTPUT 3  // IO_4
+        #define TEST_PIN_DIGITAL_IO_INPUT 2   // IO_3
+        #define TEST_PIN_PULSE 6              // IO_2
+        #define TEST_PIN_SYNC_IO 4            // IO_1
+        #define PWM_FREQUENCY_HZ 490          // PWM Frequency in Hz
+        #define TEST_PIN_ANALOG_IO_VREF A2    // Pin connected to Vdd
+        #define TEST_PIN_ANALOG_IO_DIVIDER A1 // Pin connected to voltage divider
+        #define TEST_PIN_ANALOG_IO_GND A0     // Pin connected to Ground
+        #define TEST_ADC_MAX_VALUE 1023
+        #define TEST_ADC_RESOLUTION 10 // ADC resolution
+
+        #define TEST_PIN_ANALOG_IO_DAC 53
+        #define TEST_PIN_ANALOG_IO_DAC_INPUT A3
     #elif defined(KIT_XMC14_2GO)
         #define PWM_FREQUENCY_HZ 490          // PWM Frequency in Hz
         #define TEST_PIN_ANALOG_IO_VREF A0    // Pin connected to Vdd
