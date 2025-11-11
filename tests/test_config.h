1f7bd185cef891f728e7e5ebb977d973b3c4a4d1
MDin <Dinesh.M-EE@infineon.com>
2025-11-11 15:01:44 +0530

    tests: Removed Unnecessry declaration.
    Signed-off-by: MDin <Dinesh.M-EE@infineon.com>

--- tests/test_config.h
+++ tests/test_config.h
@@ -59,9 +59,6 @@ static const float test_pwm_frequencies[] = {1, 50, 5000, 20000};
         #define TEST_PIN_ANALOG_IO_GND A0     // Pin connected to Ground
         #define TEST_ADC_MAX_VALUE 1023
         #define TEST_ADC_RESOLUTION 10 // ADC resolution
-
-        #define TEST_PIN_ANALOG_IO_DAC 53
-        #define TEST_PIN_ANALOG_IO_DAC_INPUT A3
     #elif defined(KIT_XMC14_2GO)
         #define PWM_FREQUENCY_HZ 490          // PWM Frequency in Hz
         #define TEST_PIN_ANALOG_IO_VREF A0    // Pin connected to Vdd
