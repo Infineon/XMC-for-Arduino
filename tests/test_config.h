cc3d17fc3b144beac65911edd5299386d66cefb1
zhanglinjing <Linjing.Zhang@infineon.com>
2025-11-12 10:29:37 +0100

    tests/: Update test_config.
    Signed-off-by: zhanglinjing <Linjing.Zhang@infineon.com>

--- tests/test_config.h
+++ tests/test_config.h
@@ -34,7 +34,7 @@
 
         #define TEST_PIN_ANALOG_IO_DAC 53
         #define TEST_PIN_ANALOG_IO_DAC_INPUT A3
-
+static const float test_pwm_frequencies[] = {1, 50, 5000, 50000};
     #elif defined(KIT_XMC1400_ARDUINO)
         #define TEST_PIN_DIGITAL_IO_OUTPUT 4
         #define TEST_PIN_DIGITAL_IO_INPUT 25
@@ -47,7 +47,7 @@
         #define TEST_PIN_ANALOG_IO_GND A0     // Pin connected to Ground
         #define TEST_ADC_MAX_VALUE 1023
         #define TEST_ADC_RESOLUTION 10 // ADC resolution
-
+static const float test_pwm_frequencies[] = {1, 50, 5000, 20000};
     #elif defined(KIT_XMC14_2GO)
         #define PWM_FREQUENCY_HZ 490          // PWM Frequency in Hz
         #define TEST_PIN_ANALOG_IO_VREF A0    // Pin connected to Vdd
@@ -60,6 +60,8 @@ extern XMCSPIClass SPI;
 
 // TODO: Need definition of PSOC6 for spi slave test. It should be managed more effectively.
 #if defined(ARDUINO_ARCH_PSOC6)
+static const float test_pwm_frequencies[] = {1, 50, 5000, 50000};
+
     // Test Pin Definitions
     #define TEST_PIN_SYNC_IO 4  // IO_1
     #define TEST_PIN_SPI_SSEL 3 // IO_0
