0ca8a2bdb9506e9ceadc9ab19357f1161be3ae3b
Julian Eder <julian.eder@infineon.com>
2026-06-10 16:51:16 +0200

    tools/ci.sh: Fix commit check base branch for 3.x.
    The ci_commit_formatting_run function compared against upstream/master,
    which caused verifygitlog.py to check all 700+ historical commits on
    master-2.x-3.x. Many of those were merged with noreply GitHub emails,
    making the check always fail for any feature branch based on
    master-2.x-3.x.
    
    Fix the base reference to upstream/master-2.x-3.x so only the commits
    added by a feature branch are verified. Also add --ignore-rebase to
    match the behaviour of the master branch ci.sh.
    
    Signed-off-by: Julian Eder <julian.eder@infineon.com>

--- tools/ci.sh
+++ tools/ci.sh
@@ -14,10 +14,9 @@ ulimit -n 1024
 
 function ci_commit_formatting_run {
     git remote add upstream https://github.com/Infineon/XMC-for-Arduino.git
-    git fetch --depth=100 upstream master
+    git fetch --depth=100 upstream master-2.x-3.x
     # If the common ancestor commit hasn't been found, fetch more.
-    git merge-base upstream/master HEAD || git fetch upstream master
-    # For a PR, upstream/master..HEAD ends with a merge commit into master, exclude that one.
-    python tools/verifygitlog.py -v upstream/master..HEAD --no-merges
+    git merge-base upstream/master-2.x-3.x HEAD || git fetch upstream master-2.x-3.x
+    # For a PR, upstream/master-2.x-3.x..HEAD ends with a merge commit, exclude that one.
+    python tools/verifygitlog.py --ignore-rebase -v upstream/master-2.x-3.x..HEAD --no-merges
 }
-
